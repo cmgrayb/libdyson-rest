fa83dfb4343d9be12b1aca0c8c49c862a3f416b9
Christopher <cmgrayb@outlook.com>
2025-09-05 15:21:37 -0700

    chore: bump internal version to 0.5.0a1

--- src/libdyson_rest/__init__.py
+++ src/libdyson_rest/__init__.py
@@ -28,7 +28,7 @@ Basic Usage:
         print(f"Device: {device.name} ({device.serial_number})")
 """
 
-__version__ = "0.2.0"
+__version__ = "0.5.0a1"
 __author__ = "libdyson-rest contributors"
 __email__ = "contributors@libdyson-rest.dev"
 
